12657ab575ac2e2b6526fefa6230fb6c65d6d36f
Laydo <robinsonlanden@gmail.com>
2022-04-28 17:09:22 -0700

    Button logic

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -2,24 +2,30 @@ package com.example.diceroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
 
-fun main() {
-    val myFirstDice = Dice(6)
-    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
-
-    val mySecondDice = Dice(20)
-    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
-}
-
-class Dice(val numSides: Int) {
+class Dice(private val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
     }
 }
+
